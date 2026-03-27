c321737520d3e7aeda99737765945462bd4994ea
Sara Fossati <sarafossati6@gmail.com>
2026-03-27 09:06:19 +0100

    fix: btn href

--- src/App.js
+++ src/App.js
@@ -177,7 +177,7 @@ function App() {
                   .<br /> Porta la macchina fotografica mentale e preparati a un
                   po’ di magia, natura, risate e forchette alzate.
                 </p>
-                <button className="cta-btn" type="submit"><a style={{textDecoration: 'none', color: "#ad843b"}} href="https://maps.app.goo.gl/VC5KSt6NTGm633f28" target="_blank">📍 Indicazioni Google Maps</a></button>
+                <button className="cta-btn" type="submit"><a style={{textDecoration: 'none', color: "#ad843b"}} href="https://maps.app.goo.gl/VC5KSt6NTGm633f28" target="_blank" rel="noreferrer">📍 Indicazioni Google Maps</a></button>
               </div>
             </div>
           </div>
